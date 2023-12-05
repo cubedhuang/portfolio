3641352172744630d95fa1aeaefa72007f99e68b
cubedhuang <99danielh@gmail.com>
2023-12-04 22:42:38 -0500

    add remaining projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,15 +8,68 @@
 <main class="wrapper fade">
 	<h1 class="text-6xl font-black">Projects</h1>
 
-	<p class="mt-4 text-gray-400">Some of my projects.</p>
+	<p class="mt-4 mb-8 text-gray-400">Some of my projects.</p>
 
-	<Project name="nimi.li" src="/images/projects/nimi.png">
-		Interactive dictionary for the construction Toki Pona featuring data
-		about word usage in the community.
+	<Project
+		name="nimi.li"
+		href="https://nimi.li"
+		src="/images/projects/nimi.png"
+	>
+		Interactive dictionary for Toki Pona featuring word usage and frequency
+		data from the community.
 	</Project>
 
-	<Project name="debating.live" src="/images/projects/debating.png">
+	<Project
+		name="debating.live"
+		href="https://debating.live"
+		src="/images/projects/debating.png"
+	>
 		Real-time platform for timer sharing, speech tracking, and more for
 		timed debate formats.
+	</Project>
+
+	<Project
+		name="Pseudocode Interpreter"
+		href="https://board.dan.onl"
+		src="/images/projects/pseudocode.png"
+	>
+		An interpreter for the specified pseudocode language in AP Computer
+		Science Principles.
+	</Project>
+
+	<Project
+		name="Boids"
+		href="https://boids.cubedhuang.com"
+		src="/images/projects/boids.png"
+	>
+		An interactive web-based simulation of flocking behavior with adjustable
+		parameters.
+	</Project>
+
+	<Project
+		name="Discord Code Generator"
+		href="https://embed.dan.onl"
+		src="/images/projects/embed.png"
+	>
+		An tool allowing bot developers to interactively generate code embeds in
+		various languages.
+	</Project>
+
+	<Project
+		name="Guess the Word"
+		href="https://word.dan.onl"
+		src="/images/projects/wordle.png"
+	>
+		A simple bot that uses images to accurately simulate the popular game
+		Wordle in Discord.
+	</Project>
+
+	<Project
+		name="CubeDHuang"
+		href="https://cubedhuang.com"
+		src="/images/projects/cubedhuang.jpg"
+	>
+		A collection of many small experiments and projects I've worked on over
+		the years.
 	</Project>
 </main>
--- src/routes/projects/Project.svelte
+++ src/routes/projects/Project.svelte
@@ -1,23 +1,31 @@
 <script lang="ts">
 	export let name: string;
+	export let href: string;
 	export let src: string;
 </script>
 
-<div class="group mt-8 group grid sm:grid-cols-2 rounded-xl overflow-hidden">
-	<div class="p-8 bg-gray-900 max-sm:row-start-2">
-		<h2 class="font-bold text-3xl">{name}</h2>
+<div class="mt-4">
+	<a
+		{href}
+		target="_blank"
+		rel="noopener noreferrer"
+		class="grid sm:grid-cols-2 rounded-xl overflow-hidden
+			hv:scale-[1.02] transition duration-300"
+	>
+		<div class="p-8 bg-gray-900 max-sm:row-start-2">
+			<h2 class="font-bold text-2xl">{name}</h2>
 
-		<p class="mt-4 text-gray-400">
-			<slot />
-		</p>
-	</div>
+			<p class="mt-2 text-gray-400">
+				<slot />
+			</p>
+		</div>
 
-	<div class="relative overflow-hidden max-sm:h-24">
-		<img
-			{src}
-			alt=""
-			class="absolute top-0 left-0 w-full h-full object-cover
-					group-hover:scale-105 transition duration-300"
-		/>
-	</div>
+		<div class="relative overflow-hidden max-sm:h-24">
+			<img
+				{src}
+				alt=""
+				class="absolute top-0 left-0 w-full h-full object-cover"
+			/>
+		</div>
+	</a>
 </div>
