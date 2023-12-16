b5daa89baa01431aabefc5023865892a0825e2e4
cubedhuang <99danielh@gmail.com>
2023-12-16 15:13:18 -0500

    add view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script>
 	import '../app.postcss';
+
+	import { onNavigate } from '$app/navigation';
+
 	import NavLink from './NavLink.svelte';
+
+	onNavigate(navigation => {
+		if (!document.startViewTransition) return;
+
+		return new Promise(resolve => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <nav class="wrapper mt-20 md:mt-32 text-gray-400">
@@ -14,3 +28,47 @@
 <div class="mt-12 md:mt-20 mb-20 md:mb-32">
 	<slot />
 </div>
+
+<style>
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
+	}
+
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
+	}
+
+	@keyframes slide-from-right {
+		from {
+			transform: translateX(theme(width.8));
+		}
+	}
+
+	@keyframes slide-to-left {
+		to {
+			transform: translateX(calc(-1 * theme(width.8)));
+		}
+	}
+
+	:root::view-transition-old(content) {
+		animation: 90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+	}
+
+	:root::view-transition-new(content) {
+		animation: 210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+	}
+
+	nav {
+		view-transition-name: nav;
+	}
+
+	div {
+		view-transition-name: content;
+	}
+</style>
